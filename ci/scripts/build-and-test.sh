128493402095bac9619cd2d61d0fefd40f7250a7
renodesor <renodesor@gmail.com>
2023-03-04 21:00:55 -0500

    Ajout au script la commande pour copier le pom dans le target

--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -5,4 +5,8 @@ cd git-repo
 	echo "**** Début du build maven ****"
 	./mvnw clean install
 	echo "**** Fin du build maven ****"
+	ls -l 
+	echo "**** Copie du pom ****"
+	cp pom.xml target/pom.xml
+	ls -l target
 cd ..
