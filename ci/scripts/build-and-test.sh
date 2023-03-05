18f29de84c53524a6e9e3a6f720b0e183682bae2
renodesor <renodesor@gmail.com>
2023-03-04 21:48:45 -0500

    Enlever l'afichage des parametres de luid dans le script

--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -2,14 +2,6 @@
 set -e
 
 cd git-repo
-	echo "**** Début du build maven ****"
-	echo BUILD_TEAM_NAME:${BUILD_TEAM_NAME}
-	echo BUILD_PIPELINE_NUMBER:${BUILD_PIPELINE_NUMBER}
-	echo BUILD_PIPELINE_NAME:${BUILD_PIPELINE_NAME}
-	echo BUILD_JOB_NAME:${BUILD_JOB_NAME}
-	echo BUILD_NAME:${BUILD_NAME}
-	echo uri:"http://localhost:8080/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NUMBER}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME}"
-	revision=${BUILD_NAME}
 	./mvnw clean package
 	echo "**** Fin du build maven ****"
 	ls -l 
