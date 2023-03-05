96d7b9528ba4fd888eaf5cf9e6376fa9fa082c3d
renodesor <renodesor@gmail.com>
2023-03-04 21:32:50 -0500

    Ajout de la version du build et renommage du pom

--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -3,10 +3,17 @@ set -e
 
 cd git-repo
 	echo "**** Début du build maven ****"
+	echo BUILD_TEAM_NAME:${BUILD_TEAM_NAME}
+	echo BUILD_PIPELINE_NUMBER:${BUILD_PIPELINE_NUMBER}
+	echo BUILD_PIPELINE_NAME:${BUILD_PIPELINE_NAME}
+	echo BUILD_JOB_NAME:${BUILD_JOB_NAME}
+	echo BUILD_NAME:${BUILD_NAME}
+	echo uri:"http://localhost:8080/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NUMBER}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME}"
+	SET revision=${BUILD_NAME}
 	./mvnw clean install
 	echo "**** Fin du build maven ****"
 	ls -l 
 	echo "**** Copie du pom ****"
-	cp pom.xml target/pom.xml
+	cp pom.xml target/pom-${revision}.xml
 	ls -l target
 cd ..
