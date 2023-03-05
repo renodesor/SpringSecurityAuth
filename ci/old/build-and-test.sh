520a850a94c9e4930f317a931de0cfd64ddd06ef
renodesor <renodesor@gmail.com>
2023-03-05 08:43:29 -0500

    Mise a jour sur build-and-test.sh et pipeline.yml

---
+++ ci/old/build-and-test.sh
@@ -0,0 +1,17 @@
+cd git-repo
+	echo "**** Début du build maven ****"
+	./mvnw clean package -Drevision=1.2.3
+	echo "**** Fin du build maven ****"
+	ls -l 
+	echo "**** List target ****"
+	ls -l target
+	echo "**** List target/SpringSecurityAuth-1.2.3 ****"
+	ls -l target/SpringSecurityAuth-1.2.3
+
+	echo copie du fichier war
+	cp target/SpringSecurityAuth-1.2.3.war ../distribution-repository
+	cp -R target/SpringSecurityAuth-1.2.3 ../distribution-repository/SpringSecurityAuth-1.2.3
+	echo "**** List ../distribution-repository ****"
+	ls -l ../distribution-repository
+	
+cd ..
--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -1,20 +1,49 @@
 #!/bin/sh
-set -e
-
+set -ex
 cd git-repo
-	echo "**** Début du build maven ****"
-	./mvnw clean package -Drevision=1.2.3
-	echo "**** Fin du build maven ****"
-	ls -l 
-	echo "**** List target ****"
-	ls -l target
-	echo "**** List target/SpringSecurityAuth-1.2.3 ****"
-	ls -l target/SpringSecurityAuth-1.2.3
 
-	echo copie du fichier war
-	cp target/SpringSecurityAuth-1.2.3.war ../distribution-repository
-	cp -R target/SpringSecurityAuth-1.2.3 ../distribution-repository/SpringSecurityAuth-1.2.3
-	echo "**** List ../distribution-repository ****"
-	ls -l ../distribution-repository
+#export MAVEN_USER_HOME=$(cd maven-cache && pwd)
+export COMPONENT_NAME=SpringSecurityAuth
+#export MAVEN_ARGS="-Dmaven.repo.local=../maven-cache/repository -s settings.xml ${MAVEN_ADDITIONAL_ARGS}"
+	echo No commit : $(git rev-parse --short HEAD)
+	./mvnw -version
+	if [[ -z "${VERSION}" ]]; then
+		echo version 1: ${VERSION}
+#		VERSION=$(echo "$(date +'%Y%m%d%H%M%S')-$(git rev-parse --short HEAD)") 
+		VERSION=1.0.0-build.$(echo "$(date +'%Y%m%d%H%M%S')")
+	fi 
+#	mkdir ../distribution-repository
+	echo "${VERSION}" > ../distribution-repository/version
+	cat ../distribution-repository/version
 	
+	#Verifier si jacoco-maven-plugin est présent dans pom.xml pour lancer le build
+	
+	MVN_GOAL="package"
+#	if `./mvnw help:effective-pom $MAVEN_ARGS "${MAVEN_ADDITIONAL_ARGS_ARRAY[@]}" | grep -q jacoco-maven-plugin`; then
+#		MVN_GOAL="jacoco:prepare-agent $MVN_GOAL jacoco:report" 
+#	fi 
+	MAVEN_ARGS="$MAVEN_ARGS -Drevision=${VERSION} versions: set-property -Dproperty=revision -DnewVersion=${VERSION}"
+	echo MAVEN_ARGS:${MAVEN_ARGS}
+	echo MVN_GOAL:${MVN_GOAL}
+	echo VERSION:${VERSION}
+	
+	# Ajout de la version dans la variable revision, tant pour l'exécution actuelle que pour 1 
+	#MVN_GOAL="$MVN_GOAL -Drevision=${VERSION} versions: set-property -Dproperty=revision -DnewVersion=${VERSION} -DgenerateBackupPoms=false" 
+	echo "Début du Build maven"
+	./mvnw clean package -Drevision=${VERSION}  versions:set-property -Dproperty=revision -DnewVersion=${VERSION} -DgenerateBackupPoms=false
+	#"${MAVEN_ADDITIONAL_ARGS_ARRAY[@]}"
+	echo "Fin du Build maven"
+	
+	echo -e "Copie du pom"
+	
+	cp pom.xml ../distribution-repository/${COMPONENT_NAME}-${VERSION}.pom
+	
+	# est-ce qu'il y a des artefacts à copier autres que le pom.xml? 
+	
+#	if  -G "target/${COMPONENT_NAME}-${VERSION}*.*" > /dev/null; then
+		echo -e "Copie des autres artefacts" 
+		cp target/${COMPONENT_NAME}-${VERSION}*.* ../distribution-repository/	
+		rm -f ../distribution-repository/*.original
+#	fi 
 cd ..
+
