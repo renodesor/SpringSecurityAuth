62738a9d0632552214b532edb9d938d9c7917f19
renodesor <renodesor@gmail.com>
2023-03-04 23:58:13 -0500

    copie du colis dans le repertoire de destination

--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -8,7 +8,12 @@ cd git-repo
 	ls -l 
 	echo "**** List target ****"
 	ls -l target
-	cp target/*.war /tmp/build/put/distribution-repository
+	echo creation du repertoire distribution-repository
+	mkdir /tmp/build/put/distribution-repository
+	echo copie du fichier war
+	cp target/SpringSecurityAuth-1.2.3.war /tmp/build/put/distribution-repository
 	echo "**** List distribution-repository ****"
-	ls -l distribution-repository
+	ls -l /tmp/build/put/distribution-repository
+	echo "**** List target in git-repo ****"
+	ls -l /tmp/build/put/git-repo
 cd ..
