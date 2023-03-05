3651c040f18725e0e5ea4ce20321e84b3d091b96
renodesor <renodesor@gmail.com>
2023-03-04 21:41:10 -0500

    Ajout des paramètres pour le build au fichier param

--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -9,7 +9,7 @@ cd git-repo
 	echo BUILD_JOB_NAME:${BUILD_JOB_NAME}
 	echo BUILD_NAME:${BUILD_NAME}
 	echo uri:"http://localhost:8080/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NUMBER}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME}"
-	SET revision=${BUILD_NAME}
+	revision=${BUILD_NAME}
 	./mvnw clean install
 	echo "**** Fin du build maven ****"
 	ls -l 
